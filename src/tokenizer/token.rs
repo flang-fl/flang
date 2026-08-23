162427ce17d28bfd3334c130aaab8f416ae00196
zBinFinn <ermert.finn@icloud.com>
2026-08-23 17:05:49 +0200

    refactor: tokenizer module

---
+++ src/tokenizer/token.rs
@@ -0,0 +1,54 @@
+use crate::source::Span;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Token {
+    pub span: Span,
+    pub kind: TokenKind,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TokenKind {
+    // Keywords
+    Fn,
+    If,
+    Let,
+    Mut,
+    Else,
+    Comp,
+    True,
+    False,
+    While,
+    Return,
+
+    // Symbols
+    Eq,                 // =
+    EqEq,               // ==
+    Semi,               // ;
+    Plus,               // +
+    Star,               // *
+    Bang,               // !
+    Minus,              // -
+    Slash,              // /
+    Comma,              // ,
+    Colon,              // :
+    RArrow,             // ->
+    LCurly,             // {
+    RCurly,             // }
+    LParen,             // (
+    RParen,             // )
+    LBrack,             // [
+    RBrack,             // ]
+    BangEq,             // !=
+    PlusEq,             // +=
+    StarEq,             // *=
+    MinusEq,            // -=
+    SlashEq,            // /=
+    LessThan,           // <
+    LessThanOrEqual,    // <=
+    GreaterThan,        // >
+    GreaterThanOrEqual, // >=
+
+    // Special
+    Identifier,
+    NumberLiteral,
+}
